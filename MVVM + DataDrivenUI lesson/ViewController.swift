8b297c2e8653c03c9c3ab43802887cf076602a1a
Ольга Егорова <hardcorecase@gmail.com>
2023-01-27 18:23:56 +0300

    Initial Commit

---
+++ MVVM + DataDrivenUI lesson/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM + DataDrivenUI lesson
+//
+//  Created by Ольга Егорова on 27.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
